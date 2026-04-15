0bbcc1f271556d8a6a2d64ea08882b60fb641dfe
BedrockIII <ben.owen25@yahoo.com>
2026-04-15 11:41:03 -0400

    Considered Improving the Monster Randomizer
    Added GUI for some planned randomizer settings

--- LKS Randomizer/src/RandomizerGui/MonsterRandomizerPanel.java
+++ LKS Randomizer/src/RandomizerGui/MonsterRandomizerPanel.java
@@ -6,12 +6,22 @@ import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 @SuppressWarnings("serial")
 public class MonsterRandomizerPanel extends JPanel
 {
-	JCheckBox randomize;
+	JCheckBox enabled;
+	
+	
+	
+	JCheckBox randomizeDrops;
+	JCheckBox randomizeAllDrops;
+	JCheckBox randomizeHP;
+	JTextField maxHPBox;
+	JCheckBox difficultyInsanity;
 	public MonsterRandomizerPanel()
 	{ 
 		super(new GridLayout(0, 1));
@@ -22,12 +32,60 @@ public class MonsterRandomizerPanel extends JPanel
 	    Border border = BorderFactory.createTitledBorder("Enemy Randomizer Settings");
 	    setBorder(border);
 		
-		randomize = new JCheckBox("Randomize Monsters", true);
-		randomize.setToolTipText("Enables the Monster Randomizer, disabling this will cause all job settings to have no effect");
-		add(randomize);
+		enabled = new JCheckBox("Randomize Monsters", true);
+		enabled.setToolTipText("Enables the Monster Randomizer, disabling this will cause all Monster settings to have no effect");
+		add(enabled);
+		
+		//Randomizer Mode : Group or Object
+		// Group randomizes group codes
+		//Group Settings:
+		//Include Boss Aides
+		//Include Bosses (ban invincible and some specific killers for some bosses)
+		JPanel groupSettings = new JPanel();
+		
+		
+		
+		//Object Settings:
+		//Who to randomize?
+				//Dropdown with following options
+					//Monsters Only
+					//Blockers Only
+					//Monsters and Blockers Seperately
+					//Monsters and Blockers Together
+		JPanel objectSettings = new JPanel();
+		
+		
+		JTabbedPane randomizerType = new JTabbedPane();
+		randomizerType.addTab("Group Randomization Mode", groupSettings);
+		randomizerType.addTab("Object Randomization Mode", objectSettings);
+		add(randomizerType);
+		
+		
+		//Global Settings:
+		//Randomize Drops
+		randomizeDrops = new JCheckBox("Randomize Drops", false);
+		randomizeDrops.setToolTipText("Randomize which items are dropped by slain UMA");
+		add(randomizeDrops);
+		//All Enemy Drops
+		randomizeAllDrops = new JCheckBox("Randomize All Drops", false);
+		randomizeAllDrops.setToolTipText("Randomize which UMA drop items (Only works if Randomize Drops is enabled)");
+		add(randomizeAllDrops);
+		//Randomize HP
+		randomizeHP = new JCheckBox("Randomize HP", false);
+		randomizeHP.setToolTipText("Randomize The HP of UMA");
+		add(randomizeHP);
+		//Max HP
+		maxHPBox = new JTextField("1000");
+		maxHPBox.setToolTipText("Set The Max HP for Randomized UMA (default 2500)");
+	    add(maxHPBox);
+		//Difficulty insanity(Every Difficulty is randomized Differently
+	    difficultyInsanity = new JCheckBox("Difficulty Insanity", false);
+	    difficultyInsanity.setToolTipText("Randomize Each Difficulty Seperately");
+		add(difficultyInsanity);
+		
 	}
 	public boolean enableMe()
 	{
-		return randomize.isSelected();
+		return enabled.isSelected();
 	}
 }
